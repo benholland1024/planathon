0c8ad04dbeed2d970c108c0253aa012e6533d9fc
lgreath5 <lgreath5@kent.edu>
2018-10-03 14:41:50 -0400

    Updated Register.vue
    Changed appearance and added link to login

--- src/components/Auth/Register.vue
+++ src/components/Auth/Register.vue
@@ -1,8 +1,15 @@
 <template>
 <div class="auth-popup" id="register">
+  <h2>Register</h2>
+  <p>Email:</p>
   <input v-model="email" type="text" />
-  <input v-model="password" type="password" />
-  <button @click="signIn()">Create</button>
+  <p>Password:</p>
+  <input v-model="password" type="password" /> <br><br>
+  <button @click="signIn()" class="material-button-large purple-gradient">Register</button> <br><br>
+  <p>Already have an account?</p>
+  <router-link to="/login" tag="h2" class="link light-purple faded-link">
+    Login
+  </router-link>
 </div>
 </template>
 
