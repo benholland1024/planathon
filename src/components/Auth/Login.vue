47fc99fbbc252e13a0b4443dd3024f868d3ce46d
lgreath5 <lgreath5@kent.edu>
2018-10-03 14:43:28 -0400

    Added Login.vue
    Similar to Register.vue

---
+++ src/components/Auth/Login.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="auth-popup" id="login">
+    <h2>Login</h2>
+    <p>Email:</p>
+    <input v-model="email" type="text" />
+    <p>Password:</p>
+    <input v-model="password" type="password" /> <br><br>
+    <button @click="signIn()" class="material-button-large purple-gradient">Login</button> <br><br>
+    <p>Don't have an account yet?</p>
+    <router-link to="/register" tag="h2" class="link light-purple faded-link">
+      Register
+    </router-link>
+  </div>
+</template>
+
+<script>
+import * as firebase from 'firebase';
+
+export default {
+    data() {
+        return {
+            email: "",
+            password: ""
+        }
+    },
+    methods: {
+        signIn() {
+            firebase.auth().signInWithEmailAndPassword(this.email, this.password)
+            .then((user) => {
+                console.log(user);
+            }).catch((error) => {
+                throw error;
+            });
+        }
+    }
+ }
+</script>
+
+<style scoped lang="scss">
+
+</style>
