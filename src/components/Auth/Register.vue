609533e19b2a073fab9bf30441b3ee2d0413a5f8
Brojowski <alex.gajowski@gmail.com>
2018-10-02 23:35:37 -0400

    register creates a user

--- src/components/Auth/Register.vue
+++ src/components/Auth/Register.vue
@@ -1,11 +1,32 @@
 <template>
 <div class="auth-popup" id="register">
-  
+  <input v-model="email" type="text" />
+  <input v-model="password" type="password" />
+  <button @click="signIn()">Create</button>
 </div>
 </template>
 
 <script>
+import * as firebase from 'firebase';
 
+export default {
+    data() {
+        return {
+            email: "",
+            password: ""
+        }
+    },
+    methods: {
+        signIn() {
+            firebase.auth().createUserWithEmailAndPassword(this.email, this.password)
+            .then((user) => {
+                console.log(user);
+            }).catch((error) => {
+                throw error;
+            });
+        }
+    }
+ }
 </script>
 
 <style scoped lang="scss">
