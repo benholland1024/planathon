cea247bd317175d381a036cfe8856713456b2d97
lgreath5 <lgreath5@kent.edu>
2018-10-25 19:20:48 -0400

    Edit task pop up can actually update a task in the database

--- src/components/dashboardComponents/taskAddEditModal.vue
+++ src/components/dashboardComponents/taskAddEditModal.vue
@@ -7,18 +7,18 @@
             <div style="display: flex">
               <div>
                 <p>Title:</p>
-                <input v-model="task.title" type="text" class="auth-textbox"
+                <input v-model="taskTitle" type="text" class="auth-textbox"
                       @keyup.enter=""/><br>
                 <p>Description:</p>
-                <input v-model="task.description" type="text" class="auth-textbox"
+                <input v-model="taskDesc" type="text" class="auth-textbox"
                       @keyup.enter=""/>
               </div>
               <div>
                 <p>Tags:</p>
                 <input type="checkbox" v-model="promotion">
                 <label for="checkbox">  Promotion</label><br>
-                <input type="checkbox" v-model="marketing">
-                <label for="checkbox">  Marketing</label><br>
+                <input type="checkbox" v-model="general">
+                <label for="checkbox">  General</label><br>
                 <input type="checkbox" v-model="dev">
                 <label for="checkbox">  Dev</label><br>
                 <input type="checkbox" v-model="finance">
@@ -41,8 +41,10 @@
     data() {
       return {
         showModal: false,
+        taskTitle: '',
+        taskDesc: '',
         promotion: false,
-        marketing: false,
+        general: false,
         design: false,
         dev: false,
         finance: false
@@ -62,19 +64,56 @@
       if (this.task.tags.includes("design")) {
         this.design = true;
       }
-      if (this.task.tags.includes("marketing")) {
-        this.marketing = true;
+      if (this.task.tags.includes("general")) {
+        this.general = true;
       }
       if (this.task.tags.includes("finance")) {
         this.finance = true;
       }
-      if (this.task.tags.includes("dev")) {
+      if (this.task.tags.includes("development")) {
         this.dev = true;
       }
+
+      // These variables are used for reverting changes when cancelling and edit
+      this.taskTitle = this.task.title;
+      this.taskDesc = this.task.description;
     },
     methods: {
       saveTask() {
+        var updatedTags = [];
+        if (this.promotion) {
+          updatedTags.push("promotion");
+        }
+        if (this.finance) {
+          updatedTags.push("finance");
+        }
+        if (this.general) {
+          updatedTags.push("general");
+        }
+        if (this.dev) {
+          updatedTags.push("development");
+        }
+        if (this.design) {
+          updatedTags.push("design");
+        }
 
+        var updateTaskObj = {
+         title: this.taskTitle,
+         description: this.taskDesc,
+         tags: updatedTags
+        }
+
+        this.$parent.$parent.$parent.db.collection('tasks').doc(this.task.id).update(updateTaskObj)
+        .then(() => {
+         console.log("Task saved! Nice!")
+         this.task.title = this.taskTitle;
+         this.task.description = this.taskDesc;
+         this.task.tags = updatedTags;
+
+
+        }).catch(err => {
+         console.error("error: ", err);
+        })
       }
     }
   }
