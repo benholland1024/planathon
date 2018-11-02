bedd35b7ff180695b16429c0960d89af0ee955c7
lgreath5 <lgreath5@kent.edu>
2018-11-01 21:17:52 -0400

    userOrgs getter is working

--- src/storeModules/orgs.js
+++ src/storeModules/orgs.js
@@ -5,6 +5,22 @@ const orgsDataModule = {
     firestoreRefType: 'collection', // or 'doc'
     moduleName: 'orgs',
     statePropName: 'docs',
+
+    getters: {
+      userOrgs: (state) => (userId) => {
+        var userOrgs = []
+        for (var key in state.docs) {
+          console.warn(key)
+          //console.warn(state.docs[key])
+          if (state.docs.hasOwnProperty(key)) {
+            if (state.docs[key].collaborators.includes(userId)) {
+              userOrgs.push(state.docs[key])
+            }
+          }
+        }
+        return userOrgs
+      }
+    }
     // you can also add state/getters/mutations/actions
     // for other config like fillables see 'Extra features'
   }
