6dbf02c71ef07a1d25b0c3491492470530a515d2
lunar-mycroft <lunar-mycroft@users.noreply.github.com>
2018-11-01 20:24:47 -0400

    Added basic methods for the graph2d component

--- src/components/Visualization/graph2d.vue
+++ src/components/Visualization/graph2d.vue
@@ -30,6 +30,20 @@ export default{
         this.dataset = new vis.DataSet(this.items);
         this.graph = new vis.Graph2d(this.container, this.dataset, this.options);
     },
+    methods:{
+        addDataPoint: function(point){
+            return this.dataset.add();
+        },
+        reloadData: function(newData){
+            if(!this.dataset){
+                this.dataset=new vis.DataSet(newData);
+                this.graph=new vis.Graph2d(this.container,this.dataset,this.options);
+            } else{
+                this.dataset.clear();
+                this.dataset.add(newData);
+            }
+        }
+    }
 }
 </script>
 <style scoped>
