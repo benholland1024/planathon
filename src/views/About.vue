544a19d20e0e62b76a0b944a4e7f99ecada0fae6
gabdullah <gabdull2@kent.edu>
2018-12-05 18:24:32 -0500

    About page link brings you to another page

--- src/views/About.vue
+++ src/views/About.vue
@@ -6,7 +6,7 @@
 
       <p>Planathon wasn't just based off of our experiences, though. We interviewed a wide range of hackathon organizers across the country, to provide the best range of organization suggestions possible. </p>
 
-      <p>If you're unfamiliar with the concept of a hackathon, you can learn more <a href="https://en.wikipedia.org/wiki/Hackathon">here</a>. </p>
+      <p>If you're unfamiliar with the concept of a hackathon, you can learn more <a href="https://en.wikipedia.org/wiki/Hackathon" target="_blank">here</a>. </p>
 
       <p>The Planathon team is Lauren Greathouse, Alex Gajowski, Timothy Park, Gabe Abdullah, and Ben Holland.</p>
 
